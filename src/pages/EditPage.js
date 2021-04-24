fa307ef5602b8942b1c5debf4b29dba34bfeadb5
Vaniko Akopashvili <49734341+VanoAkofashvili@users.noreply.github.com>
2021-04-24 21:54:59 +0400

    added editing functionality

--- src/pages/EditPage.js
+++ src/pages/EditPage.js
@@ -1,16 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react';
+import axios from "axios";
+import {useHistory, useParams} from "react-router-dom";
 
-const EditNote = () => {
-    return (
-        <div>
-            <form>
-                <h2>Edit Note</h2>
-                <div className="form-group">
-                <label><b>title</b></label>
-                <input type="text" className="form-control" id="formGroupExampleInput" placeholder="title" />
-                </div>
-                <button className='btn btn-primary' type="text">Add Note</button>
-            </form>    
+const EditNote = ({notes, setNotes}) => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const history = useHistory()
+  const params = useParams()
+  const note = notes.find(note => note.id === parseInt(params.id));
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    if (title || body) {
+      axios
+        .put(`http://localhost:3001/notes/${note.id}`, {...note, title, body})
+        .then(response => {
+          const newNotes = notes.map(note => note.id === response.data.id ? response.data : note)
+          setNotes(newNotes)
+          history.goBack()
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  }
+
+  return (
+    <div>
+      <form>
+        <h2>Edit Note</h2>
+        <div className="form-group">
+          <label htmlFor="title"><b>title</b></label>
+          <input type="text" value={title} className="form-control" id="title" placeholder="title"
+                 onChange={(e) => setTitle(e.target.value)}/>
+          <label htmlFor="body"><b>body</b></label>
+
+          <input type="text" value={body} className="form-control" id="body" placeholder="body"
+                 onChange={(e) => setBody(e.target.value)}/>
         </div>
-    )
+        <button onClick={handleClick} className='btn btn-primary' type="text">Edit</button>
+      </form>
+    </div>
+  )
 }
+
+export default EditNote
