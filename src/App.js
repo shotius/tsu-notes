fcd7ee69dbf0724f4bdd11fb15c914b7dcf5d7f5
Vaniko Akopashvili <49734341+VanoAkofashvili@users.noreply.github.com>
2021-04-24 19:26:34 +0400

    added functionality to remove notes

--- src/App.js
+++ src/App.js
@@ -1,46 +1,59 @@
-import React, { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Header } from './components/navbar'
-import { Sidebar } from './components/sidebar'
-import { MainPage } from './pages/MainPage'
-import { NotesPage } from './pages/NotesPage'
-import { NotePage } from './pages/NotePage'
+import React, {useState, useEffect} from 'react'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {Header} from './components/navbar'
+import {Sidebar} from './components/sidebar'
+import {MainPage} from './pages/MainPage'
+import {NotesPage} from './pages/NotesPage'
+import {NotePage} from './pages/NotePage'
 import './App.css'
 
 export default function App() {
   const [notes, setNotes] = useState([])
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
+    fetch('http://localhost:3001/notes')
       .then((response) => response.json())
       .then((data) => setNotes(data))
   }, [])
 
   const deleteNote = (id) => {
-    // const notes = notes.filter((item) => item.id !== id)
-    // setNotes(notes)
-    console.log('clicked remove')
+    fetch(`http://localhost:3001/notes/${id}`, {
+      method: 'DELETE',
+    })
+      .then(response => {
+        if (response.status < 200 || response.status > 299) {
+          throw new Error('Error deleting note')
+        } else {
+          const newNotes = notes.filter(note => note.id !== id);
+          setNotes(newNotes);
+        }
+      })
+      .catch(error => {
+        console.log(error)
+      })
+
   }
 
   const editNote = (id) => {
+
     console.log('edit is clicked')
   }
 
   return (
     <div className='App'>
       <Router>
-        <Sidebar />
-        <Header />
+        <Sidebar/>
+        <Header/>
         <div className='content'>
           <Switch>
             <Route
               path='/note/:id'
-              render={(props) => <NotePage id={props.match.params.id} />}
+              render={(props) => <NotePage id={props.match.params.id}/>}
             />
             <Route path='/notes'>
-              <NotesPage 
-                notes={notes} 
-                removeNote={deleteNote} 
+              <NotesPage
+                notes={notes}
+                removeNote={deleteNote}
                 editNote={editNote}
               />
             </Route>
@@ -48,7 +61,7 @@ export default function App() {
               <h1>About Page</h1>
             </Route>
             <Route path='/'>
-              <MainPage />
+              <MainPage/>
             </Route>
           </Switch>
         </div>
--- src/components/notes/index.js
+++ src/components/notes/index.js
@@ -7,7 +7,7 @@ export function Notes({ notes, removeNote, editNote}) {
     <div className='container some-style'>
       <ul className='list-group'>
         {notes.map((item) => (
-          <Note key={item.id} note={item} handleRemove={removeNote} handleEdit={editNote}  />
+          <Note key={item.id} note={item} handleRemove={() => removeNote(item.id)} handleEdit={editNote}  />
         ))}
       </ul>
     </div>
