57fbc5ac0b7cc7a4bee68e03fb0ee53207c13b4c
shotius <sh.archemashvili@gmail.com>
2021-04-24 20:22:38 +0400

    create form

---
+++ src/components/forms/index.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import "./style.css"
+
+export const AddNoteForm = ({handleAddition}) => {
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const newNote = {
+            title,body
+        }
+        console.log(newNote)
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className="form-group">
+            <label><b>Create New Note</b></label>
+            <input 
+                type="text"     
+                className="form-control"    
+                id="editTitle"  
+                placeholder="title"     
+                value={title}
+                onChange={({target}) => setTitle(target.value)}
+                />
+            <input 
+                type="text"     
+                className="form-control"    
+                id="editTitle"  
+                placeholder="body"     
+                value={body}
+                onChange={({target}) => setBody(target.value)}/>
+            </div>
+            
+            <button className='btn btn-primary' type="text">Add Note</button>
+        </form>
+)
+}
+
